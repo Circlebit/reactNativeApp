6e79af1b18f5a5a9cbb5075259fbfe604565730d
Circlebit <paul.seibel@mailbox.org>
2019-05-01 08:04:55 +0200

    wiring title prop and styling Add button

--- EventForm.js
+++ EventForm.js
@@ -18,22 +18,55 @@ const styles = StyleSheet.create({
         margin: 0,
         marginRight: 7,
         paddingLeft:10,
-    }
+    },
+    button: {
+        height: 50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,  
+    },
 });
 
 class EventForm extends Component {
+    state = {
+        title: null,
+        date: '',
+    };
+
     handleAddPress = () => {
+        console.log(this.state);
         this.props.navigation.navigate('list');
+    }
+
+    handleChangeTitle = (value) => {
+        this.setState({ title: value});
     }
 
     render() {
         return (
             <View style={{flex: 1}}>
                 <View style={styles.fieldContainer}>
-                    <TextInput style={styles.text}/>
+                    <TextInput 
+                        style={styles.text}
+                        placeholder="Event title"
+                        spellCheck={false}
+                        value={this.state.title}
+                        onChangeText={this.handleChangeTitle}
+                    />
                 </View>
-                <TouchableHighlight onPress={this.handleAddPress}>
-                    <Text>Add</Text>
+                <TouchableHighlight 
+                    onPress={this.handleAddPress}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Add</Text>
                 </TouchableHighlight>
             </View>
         );
